d34180344e9ad77f97cc10115fb299d2f2220994
jonchurch <me@jonchurch.com>
2018-10-30 16:58:55 -0400

    add time option, add exiting if missing required options

--- cli.js
+++ cli.js
@@ -1,14 +1,29 @@
 #!/usr/bin/env node
 const program = require('commander')
+const chalk = require('chalk')
+
 const redditDl = require('./')
 
 program
     .version('0.0.0')
     .option('-s, --subreddit <name>', 'Subreddit name, ie pics')
     .option('-f, --filter [hot, new, top]', 'Filter, hot, new, top. Default is top')
+    .option('-t, --time <value>', 'Time, one of hour, day, week, month, year, all. Default is all')
     .option('-l, --limit <value>', 'Limit, number of posts to check')
     .option('-d, --destination <value>', 'Destination folder')
     .parse(process.argv)
 
-const {subreddit, filter, limit, destination} = program
+const {subreddit, filter = "top", limit = 100, time = "all", destination} = program
+console.log({subreddit, filter, limit, time, destination})
+
+if (!subreddit) {
+    console.log(chalk.red('--subreddit is required'))
+    program.help()
+    process.exit(1)
+}
+if (!destination) {
+    console.log(chalk.red('--destination is required'))
+    program.help()
+    process.exit(1)
+}
 redditDl(subreddit, filter, limit, destination)
