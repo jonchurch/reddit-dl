c0998a2a33e73900e94f383e1842771ea6e14672
jonchurch <me@jonchurch.com>
2018-10-30 17:10:59 -0400

    add more filter sort values

--- cli.js
+++ cli.js
@@ -8,7 +8,7 @@ program
     .version('0.0.0')
     .arguments('<subreddit> <output_dir>')
     // .option('-s, --subreddit <name>', 'Subreddit name, ie pics')
-    .option('-f, --filter [hot, new, top]', 'Filter, hot, new, top. Default is top')
+    .option('-f, --filter [hot, new, top, rising, controversial, random]', 'Sort filter. Default is top')
     .option('-t, --time <value>', 'Time, one of hour, day, week, month, year, all. Default is all')
     .option('-l, --limit <value>', 'Limit, number of posts to check')
     // .option('-d, --destination <value>', 'Destination folder')
