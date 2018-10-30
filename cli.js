9291507127ee5b2730eea8e76e44e7f494176087
jonchurch <me@jonchurch.com>
2018-10-30 17:08:32 -0400

    make subreddit and output arguments instead of options

--- cli.js
+++ cli.js
@@ -6,23 +6,28 @@ const redditDl = require('./')
 
 program
     .version('0.0.0')
-    .option('-s, --subreddit <name>', 'Subreddit name, ie pics')
+    .arguments('<subreddit> <output_dir>')
+    // .option('-s, --subreddit <name>', 'Subreddit name, ie pics')
     .option('-f, --filter [hot, new, top]', 'Filter, hot, new, top. Default is top')
     .option('-t, --time <value>', 'Time, one of hour, day, week, month, year, all. Default is all')
     .option('-l, --limit <value>', 'Limit, number of posts to check')
-    .option('-d, --destination <value>', 'Destination folder')
+    // .option('-d, --destination <value>', 'Destination folder')
     .parse(process.argv)
 
-const {subreddit, filter = "top", limit = 100, time = "all", destination} = program
-console.log({subreddit, filter, limit, time, destination})
+console.log({program})
+const {filter = "top", limit = 100, time = "all"} = program
+// console.log({subreddit, filter, limit, time, destination})
+
+const [subreddit, destination] = program.args
 
 if (!subreddit) {
-    console.log(chalk.red('--subreddit is required'))
+    console.log({subreddit})
+    console.log(chalk.red('Error: --subreddit is required\n'))
     program.help()
     process.exit(1)
 }
 if (!destination) {
-    console.log(chalk.red('--destination is required'))
+    console.log(chalk.red('Error: --destination is required\n'))
     program.help()
     process.exit(1)
 }
